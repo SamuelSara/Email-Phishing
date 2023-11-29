a62f26c0967556b17b52bc9c6e83571a1e06f6b7
“Samuel <SamScott.Sarantos@calbaptist.edu>
2023-11-29 12:58:09 -0800

    The script now allows you to classify emails in TM

--- Email/src/script.py
+++ Email/src/script.py
@@ -1,55 +1,86 @@
+import os
 import pandas as pd
+import email
+import quopri
+import re
+import nltk
+import joblib
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import LabelEncoder
 
-# Load your dataset
-file_path = 'C:/Users/samsa/OneDrive/Desktop/New folder/New-Folder/Email/src/Phishing_Email.csv'
-df = pd.read_csv(file_path)
+# Ensure NLTK resources are downloaded (for text preprocessing)
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# Inspect the data for null values and class distribution
-print(df.isnull().sum())
-print(df['Email Type'].value_counts())
+def preprocess_text(text):
+    # Convert to lowercase, remove punctuation, tokenize, and remove stopwords
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+    return ' '.join(filtered_tokens)
 
-# Preprocess and clean your text data here (implement your cleaning function)
-def clean_text(text):
-    # Your cleaning process here
-    return text
+def extract_text_from_eml(file_path):
+    # Extract text content from .eml file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        msg = email.message_from_file(file)
+        text = ""
+        if msg.is_multipart():
+            for part in msg.walk():
+                ctype = part.get_content_type()
+                cdispo = str(part.get('Content-Disposition'))
+                if ctype == 'text/plain' and 'attachment' not in cdispo:
+                    text = part.get_payload(decode=True)
+                    try:
+                        text = quopri.decodestring(text).decode('utf-8')
+                    except:
+                        pass
+                    break
+        else:
+            text = msg.get_payload(decode=True)
+            try:
+                text = quopri.decodestring(text).decode('utf-8')
+            except:
+                pass
+        return text
 
-df['Email Text'] = df['Email Text'].apply(clean_text)
+if 'src' not in os.getcwd():
+    os.chdir('C:\\Users\\samsa\\OneDrive\\Desktop\\New folder\\New-Folder\\Email\\src')
 
-# Encode categorical labels to numerical labels
+# Load and prepare training data
+df = pd.read_csv('Phishing_Email.csv')
+df.dropna(inplace=True)
+df['Email Text'] = df['Email Text'].apply(preprocess_text)
 label_encoder = LabelEncoder()
-df['Email Type'] = label_encoder.fit_transform(df['Email Type'])
+y = label_encoder.fit_transform(df['Email Type'])
+X_train, X_test, y_train, y_test = train_test_split(df['Email Text'], y, test_size=0.2)
 
-# Split the data into features and target labels
-X = df['Email Text']
-y = df['Email Type']
+# Build and train the model
+pipeline = make_pipeline(TfidfVectorizer(), MultinomialNB())
+pipeline.fit(X_train, y_train)
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Save the trained model and label encoder to disk
+joblib.dump(pipeline, 'email_classifier_model.pkl')
+joblib.dump(label_encoder, 'label_encoder.pkl')
 
-# Feature extraction using TF-IDF
-tfidf_vectorizer = TfidfVectorizer(max_features=1000)
-X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-X_test_tfidf = tfidf_vectorizer.transform(X_test)
+# Load the model and label encoder
+model = joblib.load('email_classifier_model.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
 
-# Train a classifier
-classifier = MultinomialNB()
-classifier.fit(X_train_tfidf, y_train)
+def predict_eml(file_path):
+    # Predict whether an .eml file is phishing or not
+    eml_text = extract_text_from_eml(file_path)
+    preprocessed_text = preprocess_text(eml_text)
+    prediction = model.predict([preprocessed_text])
+    return label_encoder.inverse_transform(prediction)[0]
 
-# Make predictions
-y_pred = classifier.predict(X_test_tfidf)
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='weighted')
-recall = recall_score(y_test, y_pred, average='weighted')
-f1 = f1_score(y_test, y_pred, average='weighted')
-
-print(f"Accuracy: {accuracy:.2f}")
-print(f"Precision: {precision:.2f}")
-print(f"Recall: {recall:.2f}")
-print(f"F1 Score: {f1:.2f}")
+# Example usage
+eml_file_path = 'Safe.eml'  # Assuming 'Safe.eml' is in the current working directory
+result = predict_eml(eml_file_path)
+print(f"The email is classified as {result}")
